d321ab11488197514cdfb6dd3dd5a4f0d0d3ac4b
Armando Rios <armandorios.dev@gmail.com>
2025-02-16 16:11:29 -0800

    feat: add cors to project

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express")
+const cors = require("cors")
 const connectDB = require("./db")
 const taskRoutes = require("./src/routes/taskRoutes")
 const app = express()
 
 connectDB()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static("public"))
 
 app.use('/api/tasks/', taskRoutes)
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
+})
 
 module.exports = app
